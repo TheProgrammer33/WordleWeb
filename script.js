13dca66323d3a57750d4f7b0a44efcfbe88693b0
Dylan Bryant <dylan.m.bryant@gmail.com>
2023-05-21 22:12:10 -0400

    Initial Commit
    This is the start of the project code to show and get wordle solving.

---
+++ script.js
@@ -0,0 +1,40 @@
+function processInputs() {
+  var wordLength = document.getElementById("word-length").value;
+  let greenLetters = document.getElementById("green-letters").value;
+  let yellowLetters = document.getElementById("yellow-letters").value;
+  let greyLetters = document.getElementById("grey-letters").value;
+  callApiEndpoint(wordLength, greenLetters, yellowLetters, greyLetters)
+}
+
+function callApiEndpoint(wordLength, greenLetters, yellowLetters, greyLetters) {
+  var data = {
+    numberOfLetters: wordLength,
+    green: greenLetters,
+    yellow: yellowLetters,
+    grey: greyLetters
+  };
+  
+  var queryString = new URLSearchParams(data).toString();
+  var url = 'http://dylanbryant.com:5000/api/solvewordle?' + queryString;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // console.log("Response: ", data);
+      var dataBox = document.getElementById("data-box");
+      dataBox.innerHTML = "Possible Words: " + data.possibleWords + "<br>" + "Elimination Words: " + data.eliminationWords
+    })
+    .catch(error => {
+      console.log('An error occurred:', error);
+    });
+}
+
+function clearInputs() {
+  document.getElementById("word-length").value = "";
+  document.getElementById("green-letters").value = "";
+  document.getElementById("yellow-letters").value = "";
+  document.getElementById("grey-letters").value = "";
+  
+  var dataBox = document.getElementById("data-box");
+  dataBox.innerHTML = "";
+}
