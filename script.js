ef6228f202815afdcba042ca62ccecbbbfccc904
Dylan Bryant <dylan.m.bryant@gmail.com>
2023-05-21 22:40:40 -0400

    Fixed the API bug

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function callApiEndpoint(wordLength, greenLetters, yellowLetters, greyLetters) {
   };
   
   var queryString = new URLSearchParams(data).toString();
-  var url = 'http://dylanbryant.com:5000/api/solvewordle?' + queryString;
+  var url = 'http://api.dylanbryant.com:5000/api/solvewordle?' + queryString;
 
   fetch(url)
     .then(response => response.json())
