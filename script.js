e08cf11fab86054501979d6ea12c34a2c8d036bf
Dylan Bryant <dylan.m.bryant@gmail.com>
2023-05-24 00:08:37 -0400

    Validation and Notes
    Updated the page to have basic validation on inputs.
    Added a notes textarea for writing things down for solving.
    Added some styles.

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ function processInputs() {
   let greenLetters = document.getElementById("green-letters").value;
   let yellowLetters = document.getElementById("yellow-letters").value;
   let greyLetters = document.getElementById("grey-letters").value;
-  callApiEndpoint(wordLength, greenLetters, yellowLetters, greyLetters)
+  if (validateInputs(wordLength, greenLetters, yellowLetters, greyLetters)) {
+    callApiEndpoint(wordLength, greenLetters, yellowLetters, greyLetters)
+  }
 }
 
 function callApiEndpoint(wordLength, greenLetters, yellowLetters, greyLetters) {
@@ -34,7 +36,43 @@ function clearInputs() {
   document.getElementById("green-letters").value = "";
   document.getElementById("yellow-letters").value = "";
   document.getElementById("grey-letters").value = "";
+  document.getElementById("notes").value = "";
   
   var dataBox = document.getElementById("data-box");
   dataBox.innerHTML = "";
+}
+
+function validateInputs(wordLength, greenLetters, yellowLetters, greyLetters) {
+  // Currently the server only handles two word lengths
+  if (wordLength !== "5" && wordLength !== "7") {
+    document.getElementById("error-message").innerHTML = "Word Length not Supported";
+    document.getElementById("error-message").style.display = "block";
+    return false
+  } else if (greenLetters && !checkLetterAndPosition(greenLetters)) {
+    document.getElementById("error-message").innerHTML = "Invalid Green Letter input";
+    document.getElementById("error-message").style.display = "block";
+    return false
+  } else if (yellowLetters && !checkLetterAndPosition(yellowLetters)) {
+    document.getElementById("error-message").innerHTML = "Invalid Yellow Letter input";
+    document.getElementById("error-message").style.display = "block";
+    return false
+  } else if (greyLetters && !checkLettersOnly(greyLetters)) {
+    document.getElementById("error-message").innerHTML = "Invalid Grey Letter input";
+    document.getElementById("error-message").style.display = "block";
+    return false
+  } else {
+    document.getElementById("error-message").style.display = "none";
+    document.getElementById("error-message").innerHTML = "";
+    return true
+  }
+}
+
+function checkLetterAndPosition(input) {
+  let pattern = /^([A-Za-z][0-9])+$/
+  return pattern.test(input)
+}
+
+function checkLettersOnly(input) {
+  let pattern = /^([A-Za-z])+$/
+  return pattern.test(input)
 }
